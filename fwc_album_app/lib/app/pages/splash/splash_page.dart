5bdb35d63c0e50ed3400498dab3349ef6424474d
Emerson Melo Martins <emerson25xd@gmail.com>
2022-10-12 11:44:51 -0300

    Splash MVP structure with fake login

--- fwc_album_app/lib/app/pages/splash/splash_page.dart
+++ fwc_album_app/lib/app/pages/splash/splash_page.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:fwc_album_app/app/core/ui/helpers/loader.dart';
-import 'package:fwc_album_app/app/core/ui/helpers/messages.dart';
 import 'package:fwc_album_app/app/core/ui/styles/button_styles.dart';
 import 'package:fwc_album_app/app/core/ui/styles/colors_app.dart';
 import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
 import 'package:fwc_album_app/app/core/ui/widgets/button.dart';
+import 'package:fwc_album_app/app/pages/splash/presenter/splash_presenter.dart';
+import 'package:fwc_album_app/app/pages/splash/view/splash_view_impl.dart';
 
 class SplashPage extends StatefulWidget {
-  const SplashPage({super.key});
+  final SplashPresenter presenter;
+
+  const SplashPage({
+    super.key,
+    required this.presenter,
+  });
 
   @override
   State<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage>
-    with Loader<SplashPage>, Messages<SplashPage> {
+class _SplashPageState extends SplashViewImpl {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -52,7 +56,9 @@ class _SplashPageState extends State<SplashPage>
                   labelStyle:
                       context.textStyles.textSecondaryFontExtraBoldPrimaryColor,
                   label: "Acessar",
-                  onPressed: () {},
+                  onPressed: () async {
+                    await widget.presenter.checkLogin();
+                  },
                 ),
               ),
             ),
